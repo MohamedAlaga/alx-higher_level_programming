578d7107b46d00e0065ad215bf8fdf1f0bffd45b
MohamedAlaga <malaga.142014@gmail.com>
2023-11-12 22:45:41 +0200

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(len(args) - 1))
         for i in range(1, len(args)):
             print("{}: {}".format(i, args[i]))
-            
