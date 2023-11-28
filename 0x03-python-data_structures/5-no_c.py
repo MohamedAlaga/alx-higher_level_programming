fcf71e72fd0001dfbb23c955ef30425927f4db1a
MohamedAlaga <malaga.142014@gmail.com>
2023-11-28 18:18:12 +0200

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,6 @@
 def no_c(my_string ):
     newString = ""
     for idx in range(len(my_string)):
-        print( "old string is :"+ my_string+",new is "+newString )
-        if my_string[idx] != 'c' or my_string[idx] != 'C':
+        if my_string[idx] != 'c' and my_string[idx] != 'C':
             newString+=(my_string[idx])
-    my_string = newString
+    return newString
