60794292624c93f81fd61d7815835329dec99522
MohamedAlaga <malaga.142014@gmail.com>
2023-11-28 18:19:16 +0200

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def no_c(my_string ):
+def no_c(my_string):
     newString = ""
     for idx in range(len(my_string)):
         if my_string[idx] != 'c' and my_string[idx] != 'C':
-            newString+=(my_string[idx])
+            newString += (my_string[idx])
     return newString
