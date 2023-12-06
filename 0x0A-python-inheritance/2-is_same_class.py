e399aab3d5e8d78ca4c75a354d5868a058860997
MohamedAlaga <malaga.142014@gmail.com>
2023-12-06 22:40:51 +0200

    task 2-is_same_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,18 +3,10 @@
 module to check if objects fro the same class
 """
 
+
 def is_same_class(obj, a_class):
     """
     if obj and a_class are inctence of the same class return True
     otherwise return False
     """
-    return type(obj) == a_class
-    
-
-a = 1
-if is_same_class(a, int):
-    print("{} is an instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an instance of the class {}".format(a, object.__name__))
+    return type(obj) is a_class
