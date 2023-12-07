4e72478e884309127c45b742b3ca5c3d6b420f7a
MohamedAlaga <malaga.142014@gmail.com>
2023-12-07 04:14:25 +0200

    11-square.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+"""
+module for combaring ints
+"""
 
 
 class MyInt(int):
+    """
+    class inherst from int
+    """
+
     def __eq__(self, __value: object) -> bool:
+        """
+        return false if value equal
+        """
+
         return not super().__eq__(__value)
+
+    def __ne__(self, __value: object) -> bool:
+        """
+        return true if values not equivelent
+        """
+        return not super().__ne__(__value)
