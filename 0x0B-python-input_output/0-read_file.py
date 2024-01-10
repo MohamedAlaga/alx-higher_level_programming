5499d53b6b0ca8e645e9e4ea9700a30c84db8341
MohamedAlaga <malaga.142014@gmail.com>
2024-01-10 06:05:19 +0200

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module to read a file
+"""
+def read_file(filename=""):
+    """
+    function to read a file
+    """
+    with open (filename,encoding='utf-8') as f :
+        read_file = f.read()
+        return read_file
