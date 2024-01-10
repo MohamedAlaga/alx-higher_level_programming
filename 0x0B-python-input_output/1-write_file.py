5d6302317f1bc47c4ba2fab90f390e87c3f2b112
MohamedAlaga <malaga.142014@gmail.com>
2024-01-10 06:16:25 +0200

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,4 +4,7 @@ module to write to a file
 """
 def write_file(filename="", text=""):
     with open(filename,encoding="utf-8") as f :
-        f.write(text)
+        i = 0
+        while(f.write(text)):
+            i+=1
+        return i
