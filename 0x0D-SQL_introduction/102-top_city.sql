3e9c60709bd971942c4c0f6828162e35a9105913
MohamedAlaga <malaga.142014@gmail.com>
2024-01-16 23:41:11 +0200

    TASK 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+SELECT city,avg(value) AS avg_temp from hbtn_0c_0.temperatures WHERE month = 8 OR month = 7 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
