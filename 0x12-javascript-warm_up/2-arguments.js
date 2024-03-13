1f808dbebb2cb79fe7cb059045a17259117d8fcf
MohamedAlaga <malaga.142014@gmail.com>
2024-03-13 15:25:12 +0200

    second commit js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.lenght > 2) {
+  console.log('Arguments found');
+} else if (process.argv.lenght === 2) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+if (process.argv[0]) {
+  console.log(process.argv[0]);
+} else {
+  console.log('No argument');
+}
