1e677ea835e6cbcbf8c009814277e715a579ca08
MohamedAlaga <malaga.142014@gmail.com>
2024-03-14 03:07:44 +0200

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  let char = '*'
+  let char = '*';
   for (let y = 0; y < process.argv[2]; y++) {
-    char += '*'
+    char += '*';
   }
   for (let i = 0; i < process.argv[2]; i++) {
     console.log(char);
