28ed62bb5c6ef96e54ebc52ac95bb9d1780cf9d4
MohamedAlaga <malaga.142014@gmail.com>
2024-03-14 03:28:51 +0200

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,10 @@
 
 function fact (a) {
   if (a === 1 || isNaN(a)) {
-    return (1);
+    return 1;
   } else {
-    return (a * fact (a-1));
+    return a * fact (a-1);
   }
 }
+
 console.log(fact(Number(process.argv[2])));
