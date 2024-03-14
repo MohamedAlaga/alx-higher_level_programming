8826691b493722e93a5ea7d9a6834bcd3139dbb9
MohamedAlaga <malaga.142014@gmail.com>
2024-03-14 12:50:11 +0200

    102-add_me_maybe.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+//  new function incr that increments the integer value.
 
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.incr() = function () { 
+myObject.incr = function () {
   myObject.value++;
 };
 myObject.incr();
