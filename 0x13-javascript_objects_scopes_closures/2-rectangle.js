0deb3213dd920a1536ad091d79a31fff4c8f9752
MohamedAlaga <malaga.142014@gmail.com>
2024-03-16 14:49:53 +0200

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w === 0 || isNaN(w) || h === 0 || isNaN(h))
+  constructor (w, h) {
+    if (w === 0 || isNaN(w) || h === 0 || isNaN(h)) {
       this.width = w;
       this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+}
+module.exports = Rectangle;
