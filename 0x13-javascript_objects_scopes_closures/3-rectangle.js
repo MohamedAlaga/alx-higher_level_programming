1dc1ef355f439e06698ed99707330ba9a1aabb30
MohamedAlaga <malaga.142014@gmail.com>
2024-03-16 14:59:13 +0200

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+    for (let i = 0; i < this.h; i++) {
+      for (let y = 0; y < this.w; y++) {
+        str += 'X';
+      }
+      console.log(str);
+      str = '';
+    }
+  }
+}
+module.exports = Rectangle;
