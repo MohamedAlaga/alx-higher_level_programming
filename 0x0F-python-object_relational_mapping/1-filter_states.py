ecbd2b50d5d8e048de892d2104846f56498f3b8e
MohamedAlaga <malaga.142014@gmail.com>
2024-03-18 00:47:01 +0200

    0-select_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost",
+                                 user=argv[1], password=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id WHERE name = 'N%'")
+    data = cursor.fetchall()
+    for state in data:
+        print(state)
