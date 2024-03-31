7a6bacd6947dd576313f8703254b223b26a82ea5
MohamedAlaga <malaga.142014@gmail.com>
2024-03-31 12:37:06 +0200

    0x10-python-network_0 all

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+def find_peak(list_of_integers):
+    """Finds a peak in a list of integers."""
+    if not list_of_integers:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
+
