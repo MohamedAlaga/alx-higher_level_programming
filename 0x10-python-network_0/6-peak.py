29a1c8185a74f7b7d179dc3746a4ae3b9b0e8997
MohamedAlaga <malaga.142014@gmail.com>
2024-03-31 13:13:55 +0200

    0x10-python-network_0 all

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Module for finding a peak in a list of integers."""
 def find_peak(list_of_integers):
     """Finds a peak in a list of integers."""
     if not list_of_integers:
