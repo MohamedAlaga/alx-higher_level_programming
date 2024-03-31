aa4c5bb7dea80149bcc39fc683d9114ea94640f7
MohamedAlaga <malaga.142014@gmail.com>
2024-03-31 13:52:58 +0200

    0x10-python-network_0 all

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,19 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of integers."""
-    if not list_of_integers:
+    list_ = list_of_integers
+    # if there is no list of integers return None
+    if list_ == []:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    length = len(list_)
+
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+            return list_[mid]
+        if list_[mid - 1] > list_[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_[start]
