8bc4b331e51ef86d275f73a0e8ef124d3891b71f
MohamedAlaga <malaga.142014@gmail.com>
2024-07-31 05:17:11 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
 const REQ = require('request');
-let url = process.argv[2];
-REQ(url, function (error, response, body){
+const url = process.argv[2];
+REQ (url, function (error, response, body){
   if (error) {
     console.log(error);
     return;
   }
   let count = 0;
-  for (let film of JSON.parse(body).results) {
-    let characters = film.characters;
-    for (let character of characters) {
+  for (const film of JSON.parse(body).results) {
+    const characters = film.characters;
+    for (const character of characters) {
       if (character.includes('18')) {
         count++;
       }
