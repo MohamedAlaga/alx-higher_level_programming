402ce005557eb1cb7de78488bbd29d890de6e031
MohamedAlaga <malaga.142014@gmail.com>
2024-07-30 18:46:21 -0700

    '

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const REQ = require('request');
-var url = "https://swapi-api.alx-tools.com/api/films/"+process.argv[2];
-REQ(url, function (error, response, body){
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+REQ(url, function (error, response, body) {
   if (error) {
     console.log(error);
     return;
