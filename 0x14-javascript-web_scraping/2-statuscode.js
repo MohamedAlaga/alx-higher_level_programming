20683def50d4776382db524c02158103d1929d67
MohamedAlaga <malaga.142014@gmail.com>
2024-07-31 04:29:11 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const REQ = require('require');
-REQ(process.argv[2], process.argv[3], 'utf8', (error,response, body) => {
+REQ(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
     return;
