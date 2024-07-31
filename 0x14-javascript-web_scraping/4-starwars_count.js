a64b95d2b165bdc8353666e7a7c4c531ca053949
MohamedAlaga <malaga.142014@gmail.com>
2024-07-31 05:12:57 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const REQ = require('request');
-var url = process.argv[2];
+let url = process.argv[2];
 REQ(url, function (error, response, body){
   if (error) {
     console.log(error);
     return;
   }
-  var count = 0;
+  let count = 0;
   for (let film of JSON.parse(body).results) {
     let characters = film.characters;
     for (let character of characters) {
