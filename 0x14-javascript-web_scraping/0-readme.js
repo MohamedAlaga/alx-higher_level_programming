2a5d0a4b10d8f47ff22aeeb52ce152b138410e62
MohamedAlaga <malaga.142014@gmail.com>
2024-07-31 03:37:33 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    console.log(data);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
 })
